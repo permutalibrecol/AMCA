94bcf59dd8e0d417cc0e9bccb9860f2368975ea5
permutalibrecol <permutalibre@gmail.com>
2026-07-06 02:56:28 -0500

    Update site URL and base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://tusuario.github.io/amca-web-astro',
+  site: 'https://permutalibrecol.github.io/AMCA/',
+  base: '/AMCA/',
   output: 'static',
   compressHTML: true,
   build: {
