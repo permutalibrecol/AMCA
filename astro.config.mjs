93898738e882bc767539bbdd6503f529cd53b91a
permutalibrecol <permutalibre@gmail.com>
2026-07-06 11:59:45 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://permutalibrecol.github.io/AMCA/',
-  base: '/AMCA/',
-  output: 'static',
-  compressHTML: true,
-  build: {
-    inlineStylesheets: 'auto'
-  }
+  site: 'https://permutalibrecol.github.io',
+  base: '/AMCA'
 });
